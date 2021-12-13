7db9c2ab3020497972ccec3f0b034e14ec60d073
rustam <kuramshin.py@yandex.ru>
2021-12-14 02:01:37 +0300

    init commit

---
+++ src/main/java/ru/kuramshin/dev/log4jcve/Log4jCveApplication.java
@@ -0,0 +1,13 @@
+package ru.kuramshin.dev.log4jcve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jCveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4jCveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuramshin/dev/log4jcve/Log4jCveApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuramshin.dev.log4jcve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4jCveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
